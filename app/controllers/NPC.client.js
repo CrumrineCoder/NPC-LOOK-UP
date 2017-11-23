970f9596f5576a550115ea8bb0b14d1426402a08
Glitch (npclookup) <none>
2017-11-23 12:24:00 +0000

    Preparing to clone a previous project to work on Angular

--- app/controllers/NPC.client.js
+++ app/controllers/NPC.client.js
@@ -3,12 +3,5 @@
 (function(){
    console.log("Hello!");
   
-    var app = angular.module('NPCApp', []);
-    app.config(function($interpolateProvider) {
-        $interpolateProvider.startSymbol('{[{');
-        $interpolateProvider.endSymbol('}]}');
-    });
-    app.controller('NPCController', function($scope) {
-      
-    });
+   
  })
