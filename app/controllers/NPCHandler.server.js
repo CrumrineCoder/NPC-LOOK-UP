a2eb11be6ebd9ddf78cc15c270ff9cefc826f60f
Glitch (npclookup) <none>
2017-11-28 21:30:34 +0000

    Individual pages for NPCs based on IDs now gets data

--- app/controllers/NPCHandler.server.js
+++ app/controllers/NPCHandler.server.js
@@ -1,4 +1,5 @@
 'use strict';
+var ObjectId = require('mongodb').ObjectID;
 // This file gets the polls collection data from the db and handles adding votes to the database
 function NPCHandler(db) {
  var npc = db.collection('npcs');
@@ -8,6 +9,16 @@ function NPCHandler(db) {
         }).toArray(function(err, documents) {
             if (err) throw err
         
+        
+            res.json(documents);
+        })
+    }
+  this.getOneNPC = function(req, res) {
+    console.log("If I may be frank");
+    console.log(req.query.id);
+        npc.find({ _id: ObjectId(req.query.id )}).toArray(function(err, documents) {
+            if (err) throw err
+            console.log(documents);
         
             res.json(documents);
         })
