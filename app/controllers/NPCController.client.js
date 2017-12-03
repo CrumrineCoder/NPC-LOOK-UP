5676d590294a2e84e0e86bbbd602db3b1f428d0b
Glitch (npclookup) <none>
2017-12-03 18:56:39 +0000

    NPCListing get alll npcs made by a user if they're logged in

--- app/controllers/NPCController.client.js
+++ app/controllers/NPCController.client.js
@@ -31,9 +31,34 @@
             xmlhttp.open(method, url, true);
             xmlhttp.send();
         }
+       var user;
+      function getUser(callback) {
+             ajaxRequest('GET', apiUrl + "users/user_data", function(data) {
+                 data = JSON.parse(data);
+                 if (data.hasOwnProperty('username')) {
+                     user = data.username;
+                 }
+                 callback();
+             });
+         }
+      getUser(function(){ready(ajaxRequest('GET', apiUrl + "api/listings", showNPCs))});
+function search(nameKey, myArray){
+  var newArr = [];
+    for (var i=0; i < myArray.length; i++) {
+        if (myArray[i].username === nameKey && nameKey != null) {
+            newArr.push(myArray[i]);
+        }
+    }
+  return newArr; 
+}
+
+
 
         function showNPCs(data) {
+        
             var NPCObject = JSON.parse(data);
+          var resultObject = search(user, NPCObject);
+            console.log(resultObject);
             for (var i = 0; i < NPCObject.length; i++) {
                 $scope.$apply(function() {
                     $scope.NPCs.push(NPCObject[i]);
@@ -41,6 +66,6 @@
             }
 
         }
-        ready(ajaxRequest('GET', apiUrl + "api/listings", showNPCs));
+       
     });
 })();
